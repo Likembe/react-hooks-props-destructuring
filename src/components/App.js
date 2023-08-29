5dd1591b1d490558e2953ba659d90132a2b7b85a
Alpha <alpha.likembe@student.moringaschool.com>
2023-08-29 22:12:09 +0300

    review

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
+  
   const title = "Mad Max";
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
